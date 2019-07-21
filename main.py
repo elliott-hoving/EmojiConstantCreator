06bc4e757eb9f96d36142abb4aaa500358ecb1aa
Elliott Hoving <elliotthoving@gmail.com>
2019-07-21 13:08:54 -0700

    Project up and working, beautiful

--- main.py
+++ main.py
@@ -29,7 +29,12 @@ for i in range(len(tables)):
 
         # starting from index, find td element storing unicode, assign to variable
         sub_index = tables[i].find("U+", index, -1)
-        unicode = "U+" + tables[i][sub_index + 2:sub_index + 7]
+        end_of_unicode = ""
+        if tables[i].find(" ", sub_index, -1) < tables[i].find("\"", sub_index, -1):
+            end_of_unicode = tables[i].find(" ", sub_index, -1)
+        else:
+            end_of_unicode = tables[i].find("\"", sub_index, -1)
+        unicode = tables[i][sub_index:end_of_unicode]
 
         # starting from index, find first appearance of hex prefix "\x", copy hex code into variable
         sub_index = tables[i].find("\\x", index, -1)
@@ -44,9 +49,9 @@ for i in range(len(tables)):
         # create a tuple with each bit of extracted data, then add to list for later
         list_of_tuples.append((unicode, hex_code, name))
 
-        print(unicode)
-        print(hex_code)
-        print(name)
+        # print(unicode)
+        # print(hex_code)
+        # print(name)
 
         # set index to somewhere below most recent assignment, so that find() finds the next occurrence
         index = tables[i].find("<tr id=\"emoji", sub_index, -1)
@@ -77,15 +82,51 @@ for i in range(2, len(rows)):
                 name = tag[first_colon:second_colon+1]
 
         if unicode_found == False:
-            if tag.find("U+", 0, -1) != -1:
-                unicode = tag[tag.find("U+", 0, -1):tag.find("U+", 0, -1)+6]
+            sub_index = tag.find("U+", 0, -1)
+            if sub_index != -1:
+                if tag.find(" ", sub_index, -1) != -1:
+                    end_of_unicode = tag.find(" ", sub_index, -1)
+                else:
+                    end_of_unicode = tag.find("<", sub_index, -1)
+                unicode = tag[sub_index:end_of_unicode]
                 unicode_found = True
 
-    print(name)
     print(unicode)
     unicode_hash[unicode] = name
-    
+
 # at this point, HTML2 is fully analyzed and values are stored in an associative array
+
+output_file = open("output.txt", "w+")
+
+
+# write a PHP define in which a keyword defines a string
+def write_define_str(arg_1, arg_2):
+    return "define(\"" + arg_1 + "\"" + "," + "\"" + arg_2 + "\");"
+
+
+# write a PHP define in which a keyword defines a number
+def write_define_num(arg_1, arg_2):
+    return "define(\"" + arg_1 + "\"" + "," + arg_2 + ");"
+
+
+# iterate over each emoji tuple in the list
+for emoji in list_of_tuples:
+    # [0] is the unicode in tuple
+    # [1] is hex code in tuple
+    # [2] is the index of the name in the tuple
+    name = emoji[2].upper().replace(" ", "_")
+    hex_code = emoji[1]
+    output_file.write(write_define_str(name, hex_code) + "\n")
+
+    # shortname is found by taking substring which excludes opening and ending colon, uppercasing, and removing spaces
+    shortname = unicode_hash[emoji[0]][1:-1].upper().replace(" ", "_")
+
+    # replace manually, just easier than using and retaining delimiter
+    shortname = "COLON_" + shortname + "_COLON"
+
+    output_file.write(write_define_num(shortname, name))
+    # newline between each pair of lines
+    output_file.write("\n\n")
 
 
 
